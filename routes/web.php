f37c707b0e0edeefcc2e3ec08eb0b512cec57fa5
skr-mams <mario.manzanarez@sekura.mx>
2022-08-17 22:03:09 -0500

    Primeras vistas, nosotros

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('nosotros.nosotros');
 });
+Route::get('/conocenos', function () {
+    return view('nosotros.nosotros');
+});
+Route::get('/conocenos/mensaje', function () {
+    return view('nosotros.mensaje');
+});
+Route::get('/conocenos/mision', function () {
+    return view('nosotros.mision');
+});
+Route::get('/conocenos/vision', function () {
+    return view('nosotros.vision');
+});
+Route::get('/conocenos/valores', function () {
+    return view('nosotros.valores');
+});
+Route::get('/conocenos/calidad', function () {
+    return view('nosotros.calidad');
+});
+Route::get('/conocenos/organigrama', function () {
+    return view('nosotros.organigrama');
+});
+Route::get('/conocenos/directorio', function () {
+    return view('nosotros.directorio');
+});
+Route::get('/conocenos/avisos', function () {
+    return view('nosotros.avisos');
+});
+Route::get('/conocenos/normateca', function () {
+    return view('nosotros.normateca');
+});
+
