93427a63f63d7ed7a15f5b95d2be9533e76cab90
skr-mams <mario.manzanarez@sekura.mx>
2022-08-17 22:05:31 -0500

    Primeras vistas, nosotros

---
+++ resources/views/nosotros/calidad.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+@section('content')
+<div align="left" class="Estilo4 centrado col-xs-12">
+
+	<h2>Política de calidad</h2>
+	<hr class="divisor">
+	<br>
+	<img src="../permanentes/politica.jpg" align="center">
+</div>
+<br><br>
+@endSection
---
+++ resources/views/nosotros/mision.blade.php
@@ -0,0 +1,10 @@
+
+@extends('layout.layout')
+@section('content')<div class="Estilo4 centrado col-xs-12">
+    <h2>Visión y Misión</h2>
+    <hr class="divisor">
+    <br>
+    <img src="../img/muchas/misionyvision.jpg" align="center">
+</div>
+<br>
+@endSection
